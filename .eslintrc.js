32db66aa0d96a0858b8fe686fffd3eae2de390e7
Moko <moko.laboratoire@gmail.com>
2023-11-08 12:34:42 +0100

    eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react eslint-plugin-react-hooks eslint-plugin-react-native @babel/eslint-parser removed

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,5 @@ module.exports = {
     root: true,
     extends: [
       'universe/native',
-    ],
-    plugins: ['react', 'react-native', 'react-hooks', 'jsx-a11y', 'import'],
-    parser: '@babel/eslint-parser',
+    ]
   };
