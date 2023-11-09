476d594727aeb1682ad28012e5266af923a58970
Moko <moko.laboratoire@gmail.com>
2023-11-09 10:35:00 +0100

    .eslintrc.js: configuration plugins react + react-hook

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,19 @@ module.exports = {
     root: true,
     extends: [
       'universe/native',
-    ]
+      'eslint:recommended', 
+      'plugin:react/recommended', 
+      'plugin:react-hooks/recommended'
+    ],
+    parserOptions: {
+      'ecmaVersion': 12,
+      'sourceType': 'module',
+      'ecmaFeatures': {
+        'jsx': true
+      }
+    },
+    plugins: ['react', 'react-hooks'],
+    rules: {
+      
+    }
   };
